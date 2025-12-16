3b6f5eff1d3c07a2417e2dc2bbc9ed0b787955bd
Jefferson <gentbliss@gmail.com>
2025-12-16 09:41:29 +0000

    User Registration Api

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'role')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            role=validated_data['role']
+        )
+        return user
