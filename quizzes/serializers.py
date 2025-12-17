32768a8e0bf7465f620be3715e533bb1a1cc5bc1
Jefferson <gentbliss@gmail.com>
2025-12-17 08:42:54 +0000

    Updated quizzes api endpoint

--- quizzes/serializers.py
+++ quizzes/serializers.py
@@ -1,7 +1,43 @@
 from rest_framework import serializers
-from .models import Quiz
+from .models import Quiz, Question, Answer
 
-class QuizSerializer(serializers.ModelSerializer):
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('id', 'text', 'is_correct')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True)
+
+    class Meta:
+        model = Question
+        fields = ('id', 'text', 'answers')
+
+
+class QuizCreateSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+
     class Meta:
         model = Quiz
-        fields = ('id', 'title', 'description', 'created_at')
+        fields = ('id', 'title', 'description', 'questions')
+
+    def create(self, validated_data):
+        questions_data = validated_data.pop('questions')
+        quiz = Quiz.objects.create(**validated_data)
+
+        for question_data in questions_data:
+            answers_data = question_data.pop('answers')
+            question = Question.objects.create(
+                quiz=quiz,
+                **question_data
+            )
+
+            for answer_data in answers_data:
+                Answer.objects.create(
+                    question=question,
+                    **answer_data
+                )
+
+        return quiz
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Quiz
-from .serializers import QuizSerializer
+from .serializers import QuizCreateSerializer
 from .permissions import IsEducator
 
 class QuizCreateView(generics.CreateAPIView):
     queryset = Quiz.objects.all()
-    serializer_class = QuizSerializer
+    serializer_class = QuizCreateSerializer
     permission_classes = [IsAuthenticated, IsEducator]
 
     def perform_create(self, serializer):
